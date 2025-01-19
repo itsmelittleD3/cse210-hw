57364ec2278acd2c10bd3d4641a6241ec9d2d0bf
Dane <longfieldsdeen@gmail.com>
2025-01-18 17:01:02 -0700

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    string response;
+    do 
+    {
+    Random randomGenerator = new Random();
+    int secretNumber = randomGenerator.Next(1, 101);
+    int guess=0;
+    while (guess != secretNumber) {
+    Console.WriteLine("Guess the secret number: ");
+    guess = int.Parse(Console.ReadLine());
+    if (secretNumber > guess){Console.WriteLine("Higher");}
+    else if (secretNumber < guess){Console.WriteLine("Lower");}
+    else if (secretNumber == guess){Console.WriteLine("You got it!");}
+    }
+
+
+    Console.Write("Do you want to continue? ");
+    string responseraw = Console.ReadLine();
+    response = responseraw.ToLower();
+    } while(response == "yes");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello new World!");
+        //
+        string adjective = getAdjective();
+        string noun = getNoun();
+        
+        int number = Multiply(3,4);
+        Console.WriteLine($"I looked out the window saw {number} {adjective} {noun}");
+    }
+    static int Multiply(int number1,int number2){
+       return number1 * number2;
+    }
+    static string getAdjective()
+    {
+        List<string> words = new List<string>();
+        words.Add("blue");
+        words.Add("yellow");
+        words.Add("big");
+        words.Add("small");
+
+        string adjective = "blue";
+
+        return adjective;
+
+        
+    }
+
+    static string getNoun(){
+        string noun = "bird";
+        return noun;
+
     }
 }
